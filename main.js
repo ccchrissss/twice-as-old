5a0bdaab5efdd7e35ebe07b9a9e61bb7a6dd72f1
Chris Nelson <chrisnelson7791@gmail.com>
2023-10-01 18:09:49 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - 2 * sonYearsOld)
+}
+
+console.log(twiceAsOld(55, 25))
+
+// params: dadage, son age    // both numbers
+// return: number
+// e.g. twiceAsOld(55, 25) 
+// results in 5
+
+
+// absolute value of dadAge - 2 * sonAge
+
